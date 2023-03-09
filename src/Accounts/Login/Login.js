8f770664d47e1535663ad569c5b20c56aa88e74f
farukphero <farukk6319@gmail.com>
2023-03-09 23:27:17 +0600

    portfolio

--- src/Accounts/Login/Login.js
+++ src/Accounts/Login/Login.js
@@ -3,84 +3,74 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-     <div className="relative">
-     
-      <div className="relative bg-opacity-75 bg-primary">
-        <svg
-          className="absolute inset-x-0 bottom-0 text-white"
-          viewBox="0 0 1160 163"
-        >
-          <path
-            fill="currentColor"
-            d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
-          />
-        </svg>
-        <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-          <div className="flex flex-col items-center justify-between xl:flex-row">
-            <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-              “Strength does not come from physical capacity. It comes from an indomitable will.”
+      <div className="relative">
+        <div className="relative bg-opacity-75 bg-primary">
+           
+          <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="flex flex-col items-center justify-between xl:flex-row">
+              <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                  “Strength does not come from physical capacity. It comes from
+                  an indomitable will.”
                 </h2>
-              
-            </div>
-            <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
-              <div className="bg-white text-black rounded shadow-2xl p-7 sm:p-10">
-                <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
-                   Login for updates
-                </h3>
-                <form>
-                 
-                  <div className="mb-1 sm:mb-2">
-                    <label
-                      htmlFor="email"
-                      className="inline-block mb-1 font-medium"
-                    >
-                      E-mail
-                    </label>
-                    <input
-                      placeholder="Email"
-                      required
-                      type="email"
-                      className="flex-grow text-black w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                      id="email"
-                      name="email"
-                    />
-                  </div>
-                  <div className="mb-1 sm:mb-2">
-                    <label
-                      htmlFor="email"
-                      className="inline-block mb-1 font-medium"
-                    >
-                     Password
-                    </label>
-                    <input
-                      placeholder=" Password"
-                      required
-                      type="password"
-                      className="flex-grow text-black w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                      id="email"
-                      name="email"
-                    />
-                  </div>
-                  <div className="mt-4 mb-2 sm:mb-4">
-                  <button
-                      type="submit"
-                      className="font-medium  bg-gradient-to-r from-warning to-accent py-3 px-5 rounded-full w-full text-white"
-                    >
-                    Log In
-                    </button>
-                  </div>
-                  <p className="text-xs text-gray-600 sm:text-sm">
-                    We respect your privacy.
-                  </p>
-                </form>
+              </div>
+              <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
+                <div className="bg-white text-black rounded shadow-2xl p-7 sm:p-10">
+                  <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                    Login for updates
+                  </h3>
+                  <form>
+                    <div className="mb-1 sm:mb-2">
+                      <label
+                        htmlFor="email"
+                        className="inline-block mb-1 font-medium"
+                      >
+                        E-mail
+                      </label>
+                      <input
+                        placeholder="Email"
+                        required
+                        type="email"
+                        className="flex-grow text-black w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                        id="email"
+                        name="email"
+                      />
+                    </div>
+                    <div className="mb-1 sm:mb-2">
+                      <label
+                        htmlFor="email"
+                        className="inline-block mb-1 font-medium"
+                      >
+                        Password
+                      </label>
+                      <input
+                        placeholder=" Password"
+                        required
+                        type="password"
+                        className="flex-grow text-black w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                        id="email"
+                        name="email"
+                      />
+                    </div>
+                    <div className="mt-4 mb-2 sm:mb-4">
+                      <button
+                        type="submit"
+                        className="font-medium  bg-gradient-to-r from-warning to-accent py-3 px-5 rounded-full w-full text-white"
+                      >
+                        Log In
+                      </button>
+                    </div>
+                    <p className="text-xs text-gray-600 sm:text-sm">
+                      We respect your privacy.
+                    </p>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-   </div>
   );
 };
 
